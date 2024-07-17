eb1a8276ecfa7983031a96b77b45f372a7a068ba
Ritikarajwar <ritikarajwar0@gmail.com>
2024-07-17 12:04:56 +0530

    addition

--- index.js
+++ index.js
@@ -20,45 +20,45 @@ app.use(cors({ origin: "http://127.0.0.1:5500" }))
 app.use(fileUpload({ useTempFiles: true }))
 app.use(express.urlencoded({ extended: false }))
 
-app.post("/upload", async(req, res) => {
+app.post("/upload", async (req, res) => {
     let file = req.files.file;
     console.log(file);
 
-    cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
-                if (err) {
-                    console.error("this is error:" + err);
-                    res.status(500).send(JSON.stringify('Error uploading to Cloudinary'));
-                } else {
-                    console.log(result);
-                    console.log(result.secure_url)
-                    let url = result.secure_url
-                    let detail = db.collection('files').insertOne({url})
-                    res.status(200).send(JSON.stringify('File uploaded to Cloudinary'));
-                }
-            });
+    cloudinary.uploader.upload(file.tempFilePath, async (err, result) => {
+        if (err) {
+            console.error("this is error:" + err);
+            res.status(500).send(JSON.stringify('Error uploading to Cloudinary'));
+        } else {
+            console.log(result);
+            console.log(result.secure_url)
+            let url = result.secure_url
+            let publicId = result.public_id
+            console.log(publicId)
+            let detail = await db.collection('files').insertOne({ url })
+            res.status(200).send(JSON.stringify('File uploaded to Cloudinary'));
+        }
+    });
 
-    // res.json({ status: "File received" });
+
 });
 
 
+app.post("/delete", async (req, res) => {
+    console.log(req.body)
+     let publicId = req.body.publicId
 
-// cloudinary.openUploadWidget(
-//     {
-//         cloudName: "da2oqj7qe",
-//         uploadPreset: "kgqqol8b",
-//         // sources: ["local", "url", "camera"],
-//         // multiple: false,
-//         // cropping: true,
-//         // croppingAspectRatio: 1,
-//         // showSkipCropButton: false,
-//     },
-//     (error, result) => {
-//         if (!error && result && result.event === "success") {
-//             console.log("File uploaded successfully:", result.info);
-//             // Do something with the result.info object
-//         }
-//     }
-// );
+    cloudinary.uploader.destroy(publicId, function (error, result) {
+        console.log("hello")
+        if (error) {
+            console.error('Error deleting file:', error);
+        } else {
+            console.log('File deleted successfully:', result);
+        }
+    });
+
+})
+
+
 
 connection.then((client) => {
     db = client.db(dbName)
