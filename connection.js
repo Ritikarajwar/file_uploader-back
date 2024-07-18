76ce81947d09b51c87c8efb9b8e991be3635d1dc
RITIKA RAJWAR <142576192+Ritikarajwar@users.noreply.github.com>
2024-07-18 19:14:10 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,4 +1,4 @@
 import{MongoClient} from "mongodb"
-const connection = MongoClient.connect("mongodb+srv://ritikarajwar0:11223344@cluster0.u2mqpbq.mongodb.net/fileuploader?retryWrites=true&w=majority&appName=Cluster0")
+const connection = MongoClient.connect("mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/fileuploader?retryWrites=true&w=majority&appName=Cluster0")
 export const dbName = "fileuploader"
 export default connection
